c739f89c5910ac5f6b4c895a81b3f34092c50bf8
KJ Schneider <kjdschneider@gmail.com>
2019-12-13 22:32:39 -0500

    MVP complete

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,5 +1,10 @@
 import React from "react";
 
-export default function CharacterCard() {
-  return <span>todo: character</span>;
+export default function CharacterCard({char}) {
+  return (
+    <div className='char-card'>
+      <h2>{char.name}</h2>
+      <p>Gender: {char.gender}<br />Species: {char.species}<br />Status: {char.status}</p>
+    </div>
+  );
 }
--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,16 +1,41 @@
 import React, { useEffect, useState } from "react";
+import axios from 'axios';
+import CharacterCard from './CharacterCard';
+import SearchForm from './SearchForm';
 
 export default function CharacterList() {
-  // TODO: Add useState to track data from useEffect
+  const [characters, setCharacters] = useState([]);
+  const [searchTerm, setSearchTerm] = useState(' ');
+  const [searchResults, setSearchResults] = useState(characters)
+
+  
 
   useEffect(() => {
-    // TODO: Add API Request here - must run in `useEffect`
-    //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
+    axios
+      .get('https://rickandmortyapi.com/api/character/')
+      .then(res => {
+        console.log('Success: ', res);
+        setCharacters(res.data.results);
+      })
+      .catch(err => {
+        console.log('Error: ', err);
+      })
   }, []);
 
   return (
     <section className="character-list">
-      <h2>TODO: `array.map()` over your state here!</h2>
+      <SearchForm 
+        characters={characters}
+        searchTerm={searchTerm}
+        setSearchTerm={setSearchTerm}
+        setSearchResults={setSearchResults} />
+      {searchResults.map(char => {
+        return (
+          <div key={char.id} className='char-container'>
+            <CharacterCard char={char} />
+          </div>
+        )
+      })}
     </section>
   );
 }

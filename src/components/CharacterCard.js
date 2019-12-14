a37422072031ea5a17d8b4816d7b0df69872c999
KJ Schneider <kjdschneider@gmail.com>
2019-12-13 23:02:16 -0500

    Attempted a stretch goal, will leave further details in pull request

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function CharacterCard({char}) {
   return (
     <div className='char-card'>
-      <h2>{char.name}</h2>
+      <h3>{char.name}</h3>
       <p>Gender: {char.gender}<br />Species: {char.species}<br />Status: {char.status}</p>
     </div>
   );
--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,12 +1,16 @@
 import React, { useEffect, useState } from "react";
 import axios from 'axios';
 import CharacterCard from './CharacterCard';
+import LocationCard from './LocationCard';
+import EpisodeCard from './EpisodeCard';
 import SearchForm from './SearchForm';
 
 export default function CharacterList() {
   const [characters, setCharacters] = useState([]);
   const [searchTerm, setSearchTerm] = useState(' ');
   const [searchResults, setSearchResults] = useState(characters)
+  const [locations, setLocations] = useState([]);
+  const [episodes, setEpisodes] = useState([]);
 
   
 
@@ -22,20 +26,71 @@ export default function CharacterList() {
       })
   }, []);
 
+  useEffect(() => {
+    axios
+      .get('https://rickandmortyapi.com/api/location/')
+        .then(res => {
+          console.log('location success: ', res);
+          setLocations(res.results)
+        })
+        .catch(err => {
+          console.log('location error: ', err);
+        })
+  }, [])
+
+  useEffect(() => {
+    axios
+      .get('https://rickandmortyapi.com/api/episode/')
+      .then(res => {
+        console.log('episode success: ', res);
+        setEpisodes(res.results);
+      })
+      .catch(err => {
+        console.log('episodes error: ', err);
+      })
+  }, [])
+
   return (
-    <section className="character-list">
-      <SearchForm 
-        characters={characters}
-        searchTerm={searchTerm}
-        setSearchTerm={setSearchTerm}
-        setSearchResults={setSearchResults} />
-      {searchResults.map(char => {
-        return (
-          <div key={char.id} className='char-container'>
-            <CharacterCard char={char} />
-          </div>
-        )
-      })}
-    </section>
+    <div className='info'>
+
+      <section className="character-list">
+        <h2>Characters</h2>
+        <SearchForm 
+          characters={characters}
+          searchTerm={searchTerm}
+          setSearchTerm={setSearchTerm}
+          setSearchResults={setSearchResults} />
+        {searchResults.map(char => {
+          return (
+            <div key={char.id} className='char-container'>
+              <CharacterCard char={char} />
+            </div>
+          )
+        })}
+      </section>
+
+      <section className='location-list'>
+        <h2>Locations</h2>
+        {locations.map(location => {
+          return (
+            <div key={location.id} className='location-container'>
+              <LocationCard location={location} />
+            </div>
+          )
+        })}
+      </section>
+
+      <section className='episode-list'>
+        <h2>Episodes</h2>
+        {episodes.map(episode => {
+          return (
+            <div className='ep-container' key={episode.id}>
+              <EpisodeCard episode={episode} />
+            </div>
+          )
+        })}
+      </section>
+
+    </div>
   );
 }
